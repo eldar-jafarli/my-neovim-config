ab802cff3adea3b99ac8833afc961b39f9be8921
eldar-jafarli <20fantomas09@gmail.com>
2025-04-09 22:40:03 +0400

    Adjusted the LSPs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,43 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local actions = require("telescope.actions")
-        require("telescope").setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
-                    },
-                    n = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                        },
+                        n = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                        },
                     },
                 },
-            },
-        })
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end
+            })
+            local builtin = require("telescope.builtin")
+            vim.g.mapleader = " "
+            vim.g.maplocalleader = "\\"
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        end
+    },
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                            -- even more opts
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
